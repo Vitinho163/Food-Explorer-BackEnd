db5342ece4c42745b5b40f1f14bc4c21c8664f17
João Victor <victor.jvsb18@gmail.com>
2024-04-16 17:50:36 -0300

    Fix table category in database

--- src/database/migrations/02_products.js
+++ src/database/migrations/02_products.js
@@ -3,7 +3,7 @@ exports.up = knex => knex.schema.createTable("products", table => {
   table.text("name").notNullable();
   table.text("description").notNullable();
   table.integer("price").notNullable();
-  table.enum("category", ["food", "drink", "desert"]).notNullable();
+  table.enum("category", ["food", "drink", "dessert"]).notNullable();
   table.text("image")
   
   table.timestamp("created_at").defaultTo(knex.fn.now());
