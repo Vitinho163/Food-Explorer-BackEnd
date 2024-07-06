2cb088bb02f830aca3f5e8763775da91a7b42bc2
João Victor <victor.jvsb18@gmail.com>
2024-07-06 15:51:43 -0300

    Add Pix payment integration

--- src/controllers/PaymentsController.js
+++ src/controllers/PaymentsController.js
@@ -9,23 +9,23 @@ const payment = new Payment(client);
 
 class PaymentsController {
   async create(request, response) {
-    console.log(request.body)
-
     const body = { 
       transaction_amount: request.body.transaction_amount,
       description: request.body.description,
       payment_method_id: request.body.paymentMethodId,
       payer: {
         email: request.body.email,
-        identification: {
-          type: request.body.identificationType,
-          number: request.body.number
-        }
       }
     }
 
     const requestOptions = { idempotencyKey: '<SOME_UNIQUE_VALUE>' }
-    payment.create({ body, requestOptions }).then(console.log).catch(console.log);
+    payment.create({ body })
+    .then((result) => {
+      response.send(result)
+    })
+    .catch((error) => {
+      response.status(500).json({ error: error.message})
+    });
   }
 }
 
